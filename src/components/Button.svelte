f682e5fcd49b79fbf25bc5cddb883779481c8f92
Hegar Garcia <hegargarcia@gmail.com>
2020-09-24 23:39:31 -0500

    chore(component): adding button

---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  type ButtonType = "button" | "submit";
+  export let type: ButtonType = "button";
+</script>
+
+<style lang="scss">
+  button {
+    @apply w-full py-2 px-4;
+    @apply font-bold  rounded;
+
+    &:focus {
+      @apply outline-none;
+    }
+  }
+</style>
+
+<button on:click class="button" {type}>
+  <slot />
+</button>
